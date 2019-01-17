f5b3eaa94a2850ae6418692d5078e6d840468b36
Bart van Vliet <bart.van.vliet@hotmail.com>
2019-01-17 20:10:47 +0100

    :lock: Removed security for VersionController

--- src/core/VersionController.ts
+++ src/core/VersionController.ts
@@ -1,14 +1,12 @@
-import { All, Authenticated, Controller, Get, RouteService } from '@tsed/common';
-import { Docs, Security, Summary } from '@tsed/swagger';
+import { Controller, Get } from '@tsed/common';
+import { Docs, Summary } from '@tsed/swagger';
 
 @Docs('api-v1')
 @Controller('/')
 export class VersionController {
 
   @Get('/')
-  @Authenticated()
   @Summary('Receives all versions for this API')
-  @Security('token')
   public getRoutes() {
     return [
       {
